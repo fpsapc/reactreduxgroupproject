0c481031a3de7707979774991b6f7cd34a12fb30
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-05-16 23:08:51 +0500

    Fetch rockets data from given API

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import dragonReducer from './dragons/dragonSlice';
+import rocketReducer, { fetchRockets } from './rockets/rocketsSlice';
 
 const store = configureStore({
   reducer: {
+    rockets: rocketReducer,
+
     dragons: dragonReducer,
   },
 });
+
+store.dispatch(fetchRockets());
 
 export default store;
