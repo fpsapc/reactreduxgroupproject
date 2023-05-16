9dd5249c39786c1eb49ca6a963817a01bc114edf
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-05-16 05:20:21 +0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className="navBar">
+    <div className="navItems">
+      <ul className="navLinks">
+        <li>MyProfile</li>
+        <li>Missions</li>
+        <li>Rockets</li>
+      </ul>
+    </div>
+    <div className="logoTitleContainer">
+      <h2 className="navTitle">Space Travelers Hub</h2>
+      <img src="./img/logo.png" alt="logo" />
+    </div>
+  </nav>
 );
 
 export default Navbar;
