860a732764eeeace416b486ec2b6693be4352dd3
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-05-16 20:02:43 +0500

    Create Store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Missions from './components/Missions';
 
 function App() {
   return (
     <>
       <div>
         <Navbar />
+        <Missions />
         <h2>App</h2>
       </div>
     </>
